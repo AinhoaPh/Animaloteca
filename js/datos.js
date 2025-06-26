741eee42f5c177c03c69ee54561e7c7a5feb84db
AinhoaPh <ainhoiitta.phernandez@alumnos.cei.es>
2025-06-26 18:51:04 +0200

    datos. js

---
+++ js/datos.js
@@ -0,0 +1,580 @@
+
+
+
+export const madridList = [
+    {
+      categoria: "Perros y Gatos",
+      protectoras: [
+        {
+          centro: "El Refugio",
+          descripcion: "Perros y gatos rescatados del abandono y maltrato.",
+          web: "elrefugio.org",
+          telefono: "917 303 680"
+        },
+        {
+          centro: "ANAA (Asociación Nacional de Amigos de los Animales)",
+          descripcion: "Protección, rehabilitación y adopción de perros y gatos.",
+          web: "anaaweb.org",
+          telefono: "913 197 975"
+        }
+      ]
+    },
+    {
+      categoria: "Animales Domésticos (conejos, cobayas, hurones, pequeños roedores, aves domésticas)",
+      protectoras: [
+        {
+          centro: "La Madriguera",
+          descripcion: "Protección y adopción de animales pequeños y exóticos: conejos, hurones, roedores, etc.",
+          web: "lamadriguera.org"
+        },
+        {
+          centro: "APPA La Madrileña",
+          descripcion: "Acoge y da en adopción animales domésticos no convencionales.",
+          web: "facebook.com/appalamadrilena"
+        }
+      ]
+    },
+    {
+      categoria: "Aves y Aves Exóticas",
+      protectoras: [
+        {
+          centro: "Vetcon - Proyecto solidario",
+          descripcion: "Acogen aves exóticas cedidas o rescatadas.",
+          web: "vetcon.es/adopciones"
+        },
+        {
+          centro: "GREFA (también atienden aves rapaces y salvajes)",
+          descripcion: "Cría, rehabilitación y reintroducción de aves autóctonas y exóticas.",
+          web: "grefa.org"
+        }
+      ]
+    },
+    {
+      categoria: "Fauna Salvaje y Silvestre",
+      protectoras: [
+        {
+          centro: "CRAS (Centro de Recuperación de Animales Silvestres)",
+          descripcion: "Rehabilitación de fauna silvestre: erizos, zorros, reptiles, aves silvestres, etc.",
+          direccion: "Tres Cantos",
+          web: "comunidad.madrid/cras"
+        }
+      ]
+    },
+    {
+      categoria: "Fauna Exótica y Mamíferos No Domésticos",
+      protectoras: [
+        {
+          centro: "Vetcon Adopciones",
+          descripcion: "Tienen fichas de reptiles, loros, iguanas, etc.",
+          web: "vetcon.es"
+        }
+      ]
+    },
+    {
+      categoria: "Rapaces y Fauna Irrecuperable",
+      protectoras: [
+        {
+          centro: "GREFA",
+          descripcion: "Principal centro de España en aves rapaces, reptiles, mamíferos silvestres. Mantienen animales irrecuperables para educación ambiental.",
+          web: "grefa.org"
+        },
+        {
+          centro: "Kuna Ibérica",
+          descripcion: "Refugio de fauna salvaje irrecuperable. Centro educativo y de concienciación.",
+          direccion: "Navas del Rey (Madrid)",
+          web: "kunaiberica.es"
+        }
+      ]
+    }
+  ];
+
+
+  // Valencia 
+
+ export  const valenciaList = [
+    {
+      categoria: "Perros y Gatos",
+      protectoras: [
+        {
+          nombre: "SVPAP (Sociedad Valenciana Protectora de Animales y Plantas)",
+          descripcion: "Especies: Perros y gatos.",
+          direccion: "Pista de Ademuz, km 13.5, 46184 San Antonio de Benagéber, Valencia.",
+          telefono: "963 844 182",
+          web: "svpap.org"
+        }
+      ]
+    },
+    {
+      categoria: "Aves (domésticas y exóticas)",
+      protectoras: [
+        {
+          nombre: "Centro Municipal de Acogida de Especies Exóticas y Avifauna",
+          descripcion: "Especies: Aves domésticas y exóticas.",
+          direccion: "Calle Cocoters, Valencia.",
+          horario: "10:00 - 13:00 horas",
+          info: "Centro Municipal de Acogida de Especies Exóticas y Avifauna"
+        }
+      ]
+    },
+    {
+      categoria: "Fauna Exótica",
+      protectoras: [
+        {
+          nombre: "Centro Municipal de Acogida de Especies Exóticas y Avifauna",
+          descripcion: "Especies: Reptiles, loros y otros animales exóticos.",
+          direccion: "Calle Cocoters, Valencia.",
+          horario: "10:00 - 13:00 horas",
+          info: "Centro Municipal de Acogida de Especies Exóticas y Avifauna"
+        }
+      ]
+    },
+    {
+      categoria: "Fauna Salvaje y Rapaces",
+      protectoras: [
+        {
+          nombre: "Centro de Recuperación de Fauna La Granja de El Saler",
+          descripcion: "Especies: Fauna salvaje y rapaces.",
+          direccion: "El Saler, Valencia.",
+          telefono: "96 386 80 25",
+          web: "mediambient.gva.es"
+        }
+      ]
+    },
+    {
+      categoria: "Mamíferos no domésticos",
+      protectoras: [
+        {
+          nombre: "AAP Primadomus",
+          descripcion: "Especies: Primates y grandes felinos.",
+          direccion: "Villena, Alicante.",
+          web: "aap-primadomus.org"
+        }
+      ]
+    },
+    {
+      categoria: "Fauna Irrecuperable",
+      protectoras: [
+        {
+          nombre: "Kuna Ibérica",
+          descripcion: "Especies: Fauna salvaje irrecuperable.",
+          direccion: "Navas del Rey, Madrid.",
+          web: "kunaiberica.es"
+        }
+      ]
+    },
+    {
+      categoria: "Otros Animales Domésticos (caballos, burros, etc.)",
+      protectoras: [
+        {
+          nombre: "SOS Caballos Valencia",
+          descripcion: "Especies: Caballos y otros équidos.",
+          info: "SOS Caballos Valencia - Facebook"
+        }
+      ]
+    }
+  ];
+
+// Alixante 
+export   const alicanteList = [
+    {
+      categoria: "Perros y Gatos",
+      protectoras: [
+        {
+          nombre: "Protectora de Animales de Alicante (Asoka el Grande)",
+          descripcion: "Especies: Perros y gatos.",
+          direccion: "Camino Viejo de Elche, km 7, 03114 Alicante.",
+          telefono: "965 960 224",
+          web: "protectoradealicante.org"
+        }
+      ]
+    },
+    {
+      categoria: "Aves (domésticas y exóticas)",
+      protectoras: [
+        {
+          nombre: "Centro de Recuperación de Fauna de Santa Faz",
+          descripcion: "Especies: Aves domésticas y exóticas.",
+          direccion: "Santa Faz, Alicante.",
+          telefono: "Consultar sitio web",
+          web: "gva.es"
+        }
+      ]
+    },
+    {
+      categoria: "Fauna Exótica",
+      protectoras: [
+        {
+          nombre: "AAP Primadomus",
+          descripcion: "Especies: Primates y grandes felinos.",
+          direccion: "Villena, Alicante.",
+          web: "aap-primadomus.org"
+        }
+      ]
+    },
+    {
+      categoria: "Fauna Salvaje y Rapaces",
+      protectoras: [
+        {
+          nombre: "Centro de Recuperación de Fauna de Santa Faz",
+          descripcion: "Especies: Fauna salvaje y rapaces.",
+          direccion: "Santa Faz, Alicante.",
+          telefono: "Consultar sitio web",
+          web: "gva.es"
+        }
+      ]
+    },
+    {
+      categoria: "Mamíferos no domésticos",
+      protectoras: [
+        {
+          nombre: "AAP Primadomus",
+          descripcion: "Especies: Primates y grandes felinos.",
+          direccion: "Villena, Alicante.",
+          web: "aap-primadomus.org"
+        }
+      ]
+    },
+    {
+      categoria: "Fauna Irrecuperable",
+      protectoras: [
+        {
+          nombre: "AAP Primadomus",
+          descripcion: "Especies: Fauna salvaje irrecuperable.",
+          direccion: "Villena, Alicante.",
+          web: "aap-primadomus.org"
+        }
+      ]
+    },
+    {
+      categoria: "Otros Animales Domésticos",
+      protectoras: [
+        {
+          nombre: "Easy Horse Care Rescue Centre",
+          descripcion: "Especies: Caballos, ponis y burros.",
+          direccion: "Rojales, Alicante.",
+          web: "easyhorsecare.net"
+        }
+      ]
+    }
+  ];
+
+
+  // Galicia
+
+  export const galiciaList = [
+    {
+      categoria: "Perros y Gatos",
+      protectoras: [
+        {
+          nombre: "APADAN (Asociación Protectora de Animales Domésticos Abandonados de Culleredo)",
+          descripcion: "Perros y gatos.",
+          direccion: "Culleredo, A Coruña.",
+          web: "apadan.org"
+        }
+      ]
+    },
+    {
+      categoria: "Aves (domésticas y exóticas)",
+      protectoras: [
+        {
+          nombre: "Centro de Recuperación de Fauna Silvestre de Oleiros",
+          descripcion: "Aves domésticas y exóticas.",
+          direccion: "Santa Cruz de Liáns s/n, Oleiros, A Coruña.",
+          telefono: "881 960 405 / 686 597 745",
+          web: "medioambiente.xunta.gal"
+        }
+      ]
+    },
+    {
+      categoria: "Fauna Exótica",
+      protectoras: [
+        {
+          nombre: "AGAE (Asociación Galega de Adopción de Exóticos)",
+          descripcion: "Reptiles, loros y otros animales exóticos.",
+          web: "infoexoticos.wordpress.com"
+        }
+      ]
+    },
+    {
+      categoria: "Fauna Salvaje y Rapaces",
+      protectoras: [
+        {
+          nombre: "Centro de Recuperación de Fauna Silvestre de Oleiros",
+          descripcion: "Fauna salvaje y rapaces.",
+          direccion: "Santa Cruz de Liáns s/n, Oleiros, A Coruña.",
+          telefono: "881 960 405 / 686 597 745",
+          web: "medioambiente.xunta.gal"
+        }
+      ]
+    },
+    {
+      categoria: "Mamíferos no domésticos",
+      protectoras: [
+        {
+          nombre: "Centro de Recuperación de Fauna Silvestre de Oleiros",
+          descripcion: "Mamíferos salvajes autóctonos.",
+          direccion: "Santa Cruz de Liáns s/n, Oleiros, A Coruña.",
+          telefono: "881 960 405 / 686 597 745",
+          web: "medioambiente.xunta.gal"
+        }
+      ]
+    },
+    {
+      categoria: "Fauna Irrecuperable",
+      protectoras: [
+        {
+          nombre: "Centro de Recuperación de Fauna Silvestre de Oleiros",
+          descripcion: "Fauna salvaje irrecuperable.",
+          direccion: "Santa Cruz de Liáns s/n, Oleiros, A Coruña.",
+          telefono: "881 960 405 / 686 597 745",
+          web: "medioambiente.xunta.gal"
+        }
+      ]
+    },
+    {
+      categoria: "Otros Animales Domésticos (caballos, burros, etc.)",
+      protectoras: [
+        {
+          nombre: "A Granxa de Fina",
+          descripcion: "Caballos y otros équidos.",
+          web: "facebook.com/Agranxadefina"
+        }
+      ]
+    }
+  ];
+
+  // Castellon
+
+  export const castellonList = [
+    {
+      categoria: "Perros y Gatos",
+      protectoras: [
+        {
+          nombre: "Huellas Callejeras",
+          descripcion: "Perros y gatos.",
+          direccion: "Castellón de la Plana.",
+          web: "huellascallejeras.com"
+        },
+        {
+          nombre: "ASPAC (Amigos de los Animales de Castellón)",
+          descripcion: "Perros y gatos.",
+          direccion: "Castellón de la Plana.",
+          web: "aspac.org.es"
+        },
+        {
+          nombre: "Refugio Alcolea",
+          descripcion: "Perros y gatos.",
+          direccion: "Almazora, Castellón.",
+          web: "refugio-alcolea.ueniweb.com"
+        },
+        {
+          nombre: "Protectora El Cau",
+          descripcion: "Perros.",
+          direccion: "Vinaròs, Castellón.",
+          web: "protectoraelcau.com"
+        },
+        {
+          nombre: "Tara Verde",
+          descripcion: "Perros y gatos.",
+          direccion: "Burriana, Castellón.",
+          web: "facebook.com/Protectora-de-Animales-Tara-Verde"
+        }
+      ]
+    },
+    {
+      categoria: "Aves (domésticas y exóticas)",
+      protectoras: [
+        {
+          nombre: "Centro de Recuperación de Fauna La Granja (El Saler)",
+          descripcion: "Aves domésticas y exóticas.",
+          direccion: "El Saler, Valencia (servicio regional).",
+          web: "parquesnaturales.gva.es"
+        }
+      ]
+    },
+    {
+      categoria: "Fauna Exótica",
+      protectoras: [
+        {
+          nombre: "Centro de Recuperación de Fauna La Granja (El Saler)",
+          descripcion: "Reptiles, loros y otros animales exóticos.",
+          direccion: "El Saler, Valencia.",
+          web: "parquesnaturales.gva.es"
+        }
+      ]
+    },
+    {
+      categoria: "Fauna Salvaje y Rapaces",
+      protectoras: [
+        {
+          nombre: "Centro de Recuperación de Fauna La Granja (El Saler)",
+          descripcion: "Fauna salvaje y rapaces.",
+          direccion: "El Saler, Valencia.",
+          web: "parquesnaturales.gva.es"
+        }
+      ]
+    },
+    {
+      categoria: "Mamíferos no domésticos",
+      protectoras: [
+        {
+          nombre: "Centro de Recuperación de Fauna La Granja (El Saler)",
+          descripcion: "Mamíferos salvajes autóctonos.",
+          direccion: "El Saler, Valencia.",
+          web: "parquesnaturales.gva.es"
+        }
+      ]
+    },
+    {
+      categoria: "Fauna Irrecuperable",
+      protectoras: [
+        {
+          nombre: "Centro de Recuperación de Fauna La Granja (El Saler)",
+          descripcion: "Fauna salvaje irrecuperable.",
+          direccion: "El Saler, Valencia.",
+          web: "parquesnaturales.gva.es"
+        }
+      ]
+    }
+  ];
+
+
+  export const castillaLaManchaList = [
+    {
+      categoria: "Perros y Gatos",
+      protectoras: [
+        {
+          centro: "Protectora de Animales La Bienvenida",
+          descripcion: "Especies: Perros y gatos.",
+          direccion: "Ciudad Real",
+          sitioWeb: "protectoralabienvenida.org"
+        },
+        {
+          centro: "Asociación Protectora de Animales de Toledo (APAT)",
+          descripcion: "Especies: Perros y gatos.",
+          direccion: "Toledo",
+          sitioWeb: "apattoledo.org"
+        },
+        {
+          centro: "Cuencanimal",
+          descripcion: "Especies: Perros y gatos.",
+          direccion: "Cuenca",
+          sitioWeb: "cuencanimal.com"
+        },
+        {
+          centro: "Protectora de Animales de Guadalajara",
+          descripcion: "Especies: Perros y gatos.",
+          direccion: "Guadalajara",
+          sitioWeb: "protectoraguadalajara.org"
+        },
+        {
+          centro: "Protectora de Animales de Albacete",
+          descripcion: "Especies: Perros y gatos.",
+          direccion: "Albacete",
+          sitioWeb: "protectoraalbacete.org"
+        }
+      ]
+    },
+    {
+      categoria: "Aves (domésticas y exóticas)",
+      protectoras: [
+        {
+          centro: "Centro de Recuperación de Fauna Silvestre El Chaparrillo",
+          descripcion: "Especies: Aves domésticas y exóticas.",
+          direccion: "Ciudad Real",
+          sitioWeb: "chaparrillo.castillalamancha.es"
+        },
+        {
+          centro: "Centro de Recuperación de Fauna Silvestre de Albacete",
+          descripcion: "Especies: Aves domésticas y exóticas.",
+          direccion: "Albacete",
+          sitioWeb: "castillalamancha.es"
+        }
+      ]
+    },
+    {
+      categoria: "Fauna Exótica",
+      protectoras: [
+        {
+          centro: "Centro de Recuperación de Fauna Silvestre de Cuenca",
+          descripcion: "Especies: Reptiles, loros y otros animales exóticos.",
+          direccion: "Cuenca",
+          sitioWeb: "castillalamancha.es"
+        },
+        {
+          centro: "Centro de Recuperación de Fauna Silvestre de Guadalajara",
+          descripcion: "Especies: Reptiles, loros y otros animales exóticos.",
+          direccion: "Guadalajara",
+          sitioWeb: "zoo.guadalajara.es"
+        }
+      ]
+    },
+    {
+      categoria: "Fauna Salvaje y Rapaces",
+      protectoras: [
+        {
+          centro: "Centro de Estudios de Rapaces Ibéricas (CERI)",
+          descripcion: "Especies: Fauna salvaje y rapaces.",
+          direccion: "Sevilleja de la Jara, Toledo",
+          sitioWeb: "geacam.es"
+        },
+        {
+          centro: "Parque Cinegético Experimental de El Hosquillo",
+          descripcion: "Especies: Fauna salvaje y rapaces.",
+          direccion: "Cuenca",
+          sitioWeb: "es.wikipedia.org"
+        }
+      ]
+    },
+    {
+      categoria: "Mamíferos no domésticos",
+      protectoras: [
+        {
+          centro: "Centro de Recuperación de Fauna Silvestre de Albacete",
+          descripcion: "Especies: Mamíferos salvajes autóctonos.",
+          direccion: "Albacete",
+          sitioWeb: "castillalamancha.es"
+        },
+        {
+          centro: "Centro de Recuperación de Fauna Silvestre de Cuenca",
+          descripcion: "Especies: Mamíferos salvajes autóctonos.",
+          direccion: "Cuenca",
+          sitioWeb: "castillalamancha.es"
+        }
+      ]
+    },
+    {
+      categoria: "Fauna Irrecuperable",
+      protectoras: [
+        {
+          centro: "Centro de Recuperación de Fauna Silvestre El Chaparrillo",
+          descripcion: "Especies: Fauna salvaje irrecuperable.",
+          direccion: "Ciudad Real",
+          sitioWeb: "chaparrillo.castillalamancha.es"
+        },
+        {
+          centro: "Centro de Estudios de Rapaces Ibéricas (CERI)",
+          descripcion: "Especies: Fauna salvaje irrecuperable.",
+          direccion: "Sevilleja de la Jara, Toledo",
+          sitioWeb: "geacam.es"
+        }
+      ]
+    },
+    {
+      categoria: "Otros Animales Domésticos (caballos, burros, etc.)",
+      protectoras: [
+        {
+          centro: "Santuario La Frontera",
+          descripcion: "Especies: Caballos, burros y otros animales de granja.",
+          direccion: "Ciudad Real",
+          sitioWeb: "teaming.net"
+        },
+        {
+          centro: "Santuario Happyland",
+          descripcion: "Especies: Caballos, burros y otros animales de granja.",
+          direccion: "Guadalajara",
+          sitioWeb: "madridvegano.es"
+        }
+      ]
+    }
+  ];
